b18aebc57850329a4deab0714bb3d1f7e43d3ea7
Bert Bruggeman <bbruggeman1974@gmail.com>
2022-08-19 17:04:08 +0200

    versions

--- bin/unpub_server_bb.dart
+++ bin/unpub_server_bb.dart
@@ -2,7 +2,7 @@ import 'package:mongo_dart/mongo_dart.dart';
 import 'package:unpub/unpub.dart' as unpub;
 
 main(List<String> args) async {
-  final db = Db('mongodb://localhost:27017/dart_pub');
+  final db = Db('mongodb://mongo4:27017/dart_pub');
   await db.open(); // make sure the MongoDB connection opened
 
   final app = unpub.App(
